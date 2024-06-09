18a6a05f2ba27b69e777819d50e5822a463e73f2
Stan-breaks <stanleymwendwa03@gmail.com>
2024-06-09 06:30:28 +0300

    clean up of the main file

--- app/main.go
+++ app/main.go
@@ -1,153 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"compress/gzip"
+	"./models"
+	"./router"
 	"fmt"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 )
-
-type Request struct {
-	Method  string
-	Path    string
-	Headers map[string]string
-	Body    string
-}
-
-func handleget(conn net.Conn, request Request) {
-	defer conn.Close()
-	err := error(nil)
-	if request.Path == "/" {
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		if err != nil {
-			fmt.Println("Error writing response:", err.Error())
-			return
-		}
-	} else if request.Path[0:6] == "/echo/" {
-		echo := request.Path[6:]
-		fileEncoding, ok := request.Headers["Accept-Encoding"]
-		if ok {
-			buffer := new(bytes.Buffer)
-			writer := gzip.NewWriter(buffer)
-			_, err = writer.Write([]byte(echo))
-			if err != nil {
-				fmt.Println("Error while writing ", err.Error())
-			}
-			writer.Close()
-			_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprintf("%d", len(buffer.Bytes())) + "\r\nContent-Encoding: " + fileEncoding + "\r\n\r\n" + buffer.String()))
-		} else {
-			_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprintf("%d", len(echo)) + "\r\n\r\n" + echo))
-		}
-		if err != nil {
-			fmt.Println("Error writing response:", err.Error())
-			return
-		}
-	} else if request.Path[0:7] == "/files/" {
-		filepath := request.Path[7:]
-		dir := os.Args[2]
-		data, err := os.ReadFile(dir + "/" + filepath)
-		if err != nil {
-			_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-			if err != nil {
-				fmt.Println("Error writing response:", err.Error())
-				return
-			}
-		} else {
-			_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + strconv.Itoa(len(data)) + "\r\n\r\n" + string(data)))
-			if err != nil {
-				fmt.Println("Error writing response:", err.Error())
-				return
-			}
-		}
-	} else if request.Path == "/user-agent" {
-		userAgent := request.Headers["User-Agent"]
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprintf("%d", len(userAgent)) + "\r\n\r\n" + userAgent))
-		if err != nil {
-			fmt.Println("Error writing response:", err.Error())
-			return
-		}
-	} else {
-		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		if err != nil {
-			fmt.Println("Error writing response:", err.Error())
-			return
-		}
-	}
-}
-
-func handlepost(conn net.Conn, request Request) {
-	defer conn.Close()
-	if request.Path[0:7] == "/files/" {
-		filepath := request.Path[7:]
-		dir := os.Args[2]
-		data := request.Body
-		err := os.WriteFile(dir+"/"+filepath, []byte(data+"\n"), 0644)
-		if err != nil {
-			fmt.Println("Error writing into file", err.Error())
-			return
-		}
-		_, err = conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
-		if err != nil {
-			fmt.Println("Error writing response:", err.Error())
-			return
-		}
-	}
-}
-
-func handleput(conn net.Conn, request Request) {
-	defer conn.Close()
-	if request.Path[0:7] == "/files/" {
-		filepath := request.Path[7:]
-		dir := os.Args[2]
-		writeData := request.Body
-		readData, err := os.ReadFile(dir + "/" + filepath)
-		if err != nil {
-			_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-			if err != nil {
-				fmt.Println("Error writing response:", err.Error())
-				return
-			}
-		} else {
-			buffer := new(bytes.Buffer)
-			buffer.Write(readData)
-			buffer.Write([]byte(writeData + "\n"))
-			data := buffer.Bytes()
-			err = os.WriteFile(dir+"/"+filepath, data, 0644)
-			if err != nil {
-				fmt.Println("Error with writing to file :", err.Error())
-				return
-			}
-			_, err = conn.Write([]byte("HTTP/1.1 204 Put Successfull\r\n\r\n"))
-			if err != nil {
-				fmt.Println("Error writing response:", err.Error())
-				return
-			}
-		}
-	}
-}
-
-func handledelete(conn net.Conn, request Request) {
-	defer conn.Close()
-	if request.Path[0:7] == "/files/" {
-		filepath := request.Path[7:]
-		dir := os.Args[2]
-		err := os.Remove(dir + "/" + filepath)
-		if err != nil {
-			_, err = conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
-			fmt.Println("Error with deleting the file", err.Error())
-			return
-		}
-		_, err = conn.Write([]byte("HTTP/1.1 204 No Content\r\n\r\n"))
-		if err != nil {
-			fmt.Println("Error writing response:", err.Error())
-			return
-		}
-
-	}
-}
 
 func handleconnection(conn net.Conn) {
 	defer conn.Close()
@@ -159,7 +19,7 @@ func handleconnection(conn net.Conn) {
 	}
 	fmt.Println("Received data: ", string(buf))
 	lines := strings.Split(string(buf), "\r\n")
-	request := Request{
+	request := models.Request{
 		Method: strings.Trim(strings.Split(lines[0], " ")[0], " "),
 		Path:   strings.Split(lines[0], " ")[1],
 		Headers: func() map[string]string {
@@ -176,23 +36,7 @@ func handleconnection(conn net.Conn) {
 		}(),
 		Body: strings.Trim(strings.Split(string(buf), "\r\n\r\n")[1], "\x00"),
 	}
-	if request.Method == "GET" {
-		handleget(conn, request)
-	} else if request.Method == "PUT" {
-		handleput(conn, request)
-	} else if request.Method == "POST" {
-		handlepost(conn, request)
-	} else if request.Method == "DELETE" {
-		handledelete(conn, request)
-	} else {
-		fmt.Println("Invalid request method")
-		_, err = conn.Write([]byte("HTTP/1.1 500 Internal Server Error"))
-		if err != nil {
-			fmt.Println("Error writing response:", err.Error())
-			return
-
-		}
-	}
+	router.Route(conn, request)
 }
 
 func main() {
